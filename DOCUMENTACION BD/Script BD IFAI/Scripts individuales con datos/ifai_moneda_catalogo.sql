dfecf79b91e6ddef2c7be0873babc0adda4c52ef
AlanRM <rosasaerm@gmail.com>
2014-09-23 20:18:24 -0500

    Se modifica columnas CREATED_DATE Y UPDATED_DATE por CREATED_AT Y UPDATED_AT y se establece el valor por default de las mismas a un valor distinto de null ademas se crean scripts de carga individuales

---
+++ DOCUMENTACION BD/Script BD IFAI/Scripts individuales con datos/ifai_moneda_catalogo.sql
@@ -0,0 +1,55 @@
+CREATE DATABASE  IF NOT EXISTS `ifai` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `ifai`;
+-- MySQL dump 10.13  Distrib 5.6.13, for Win32 (x86)
+--
+-- Host: localhost    Database: ifai
+-- ------------------------------------------------------
+-- Server version	5.6.15-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `moneda_catalogo`
+--
+
+DROP TABLE IF EXISTS `moneda_catalogo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `moneda_catalogo` (
+  `idMoneda` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Llave identificadora del catalogo de monedas',
+  `MONEDA` varchar(10) NOT NULL COMMENT 'Campo que indica la descripcion de la moneda',
+  PRIMARY KEY (`idMoneda`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `moneda_catalogo`
+--
+
+LOCK TABLES `moneda_catalogo` WRITE;
+/*!40000 ALTER TABLE `moneda_catalogo` DISABLE KEYS */;
+INSERT INTO `moneda_catalogo` VALUES (1,'MXP\r');
+INSERT INTO `moneda_catalogo` VALUES (2,'USD\r');
+INSERT INTO `moneda_catalogo` VALUES (3,'EURO\r');
+/*!40000 ALTER TABLE `moneda_catalogo` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2014-09-23 20:17:05
