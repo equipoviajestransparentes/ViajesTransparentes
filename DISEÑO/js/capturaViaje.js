fb056827d2787fe0c55d7525e341619d3de2af38
magymora <magy.mango84@gmail.com>
2014-09-23 19:42:26 -0500

    Formulario de comision campos completos

--- DISEÑO/js/capturaViaje.js
+++ DISEÑO/js/capturaViaje.js
@@ -1,6 +1,64 @@
-var app = angular.module('ModuloCaptura',['ui.bootstrap']);
+var app = angular.module('ModuloCaptura',['ui.bootstrap'])
+	.controller('AccordionDemoCtrl', function($scope){
+	  $scope.oneAtATime = true;
 
-function AccordionDemoCtrl($scope) {
+	  $scope.status = {
+		isFirstOpen: true,
+		isFirstDisabled: false
+	  };
+	  
+	  
+	})
+
+app.controller('FormCtrlComision',function($scope){
+	$scope.guardar = function() {
+         
+    };
+	
+	$scope.reset();
+})
+
+app.controller('atepickerDemoCtrl',function ($scope){
+  $scope.today = function() {
+    $scope.dt = new Date();
+  };
+  $scope.today();
+
+  $scope.clear = function () {
+    $scope.dt = null;
+  };
+
+  // Disable weekend selection
+  $scope.disabled = function(date, mode) {
+    return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
+  };
+
+  $scope.toggleMin = function() {
+    $scope.minDate = $scope.minDate ? null : new Date();
+  };
+  $scope.toggleMin();
+
+  $scope.open = function($event) {
+    $event.preventDefault();
+    $event.stopPropagation();
+
+    $scope.opened = true;
+  };
+
+  $scope.dateOptions = {
+    formatYear: 'yy',
+    startingDay: 1
+  };
+
+  $scope.initDate = new Date('2016-15-20');
+  $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+  $scope.format = $scope.formats[0];
+})
+
+
+
+
+/*function AccordionDemoCtrl($scope) {
   $scope.oneAtATime = true;
 
   $scope.groups = [
@@ -25,4 +83,5 @@ function AccordionDemoCtrl($scope) {
     isFirstOpen: true,
     isFirstDisabled: false
   };
-}
+}*/
+
