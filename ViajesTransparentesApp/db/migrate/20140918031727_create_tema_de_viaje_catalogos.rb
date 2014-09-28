a1d73ec66d470636acac02c313a1d65c757deb69
MagyMoRa <magy.mango84@gmail.com>
2014-09-27 20:03:24 -0400

    borrados

--- ViajesTransparentesApp/db/migrate/20140918031727_create_tema_de_viaje_catalogos.rb
+++
@@ -1,7 +0,0 @@
-class CreateTemaDeViajeCatalogos < ActiveRecord::Migration
-  def change
-    create_table :tema_viaje_cats do |t|
-      t.string :tema
-    end
-  end
-end
--- ViajesTransparentesApp/db/migrate/20140927180830_agregar_columna_num_comision_a_commsions.rb
+++
@@ -1,5 +0,0 @@
-class AgregarColumnaNumComisionACommsions < ActiveRecord::Migration
-  def change
-  	add_column :commissions, :num_comision, :string
-  end
-end
